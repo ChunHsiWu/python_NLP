1cabacafb0fe405ca378f8e2d6a939583b2ceeb5
Eddie_wu <chw396@uowmail.edu.au>
2016-11-29 23:34:48 +1100

    Test NLTK functionality

--- python_NLP/main.py
+++ python_NLP/main.py
@@ -1,5 +1,21 @@
+### Import models
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+### define functions
+
+
 def main():
-    print("program strat!!")
+    example_sentence = "this is an example showing off stop word filteration"
+    stop_words = set(stopwords.words("english"))    # get english stop words
+    words = word_tokenize(example_sentence)
+    print(stop_words)
+    filtered_sentence = [w for w in words if w not in stop_words]
+
+    print(filtered_sentence)
+
+
 if __name__ == "__main__":
     main()
 else:
